aa2ce5a6f9b078fbaa60c858482ef7b1659208c4
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 20:41:34 +0530

    adding RSURNode class

--- RectSideUpRectStage.ts
+++ RectSideUpRectStage.ts
@@ -138,3 +138,48 @@ class Animator {
         }
     }
 }
+
+class RSURNode {
+
+    prev : RSURNode
+    next : RSURNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RSURNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRSURNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RSURNode {
+        var curr : RSURNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
