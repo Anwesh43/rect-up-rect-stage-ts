46245da7d397ac83e1531a8f451f9ca5cabbaeb4
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 20:18:01 +0530

    adding ScaleUtil class

--- RectSideUpRectStage.ts
+++ RectSideUpRectStage.ts
@@ -9,6 +9,30 @@ const sizeFactor : number = 2.9
 const foreColor : string = "#673AB7"
 const backColor : string = "#bdbdbd"
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number)  : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static scaleFactor(scale : number) : number {
+        return Math.floor(scale / scDiv)
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) : number {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap 
+    }
+}
+
 class RectSideUpRectStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
@@ -38,5 +62,4 @@ class RectSideUpRectStage {
         stage.render()
         stage.handleTap()
     }
-
 }
