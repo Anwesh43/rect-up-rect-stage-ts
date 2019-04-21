676ed76a1cb952464b03a65e7863ca3ebd3b290f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 20:31:00 +0530

    adding DrawingUtil class

--- RectSideUpRectStage.ts
+++ RectSideUpRectStage.ts
@@ -8,6 +8,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#673AB7"
 const backColor : string = "#bdbdbd"
+const rectSizeFactor : number = 0.66
 
 class ScaleUtil {
 
@@ -29,7 +30,38 @@ class ScaleUtil {
     }
 
     static updateValue(scale : number, dir : number, a : number, b : number) : number {
-        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap 
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
+
+class DrawingUtil {
+
+    static drawUpRect(context : CanvasRenderingContext2D, size : number, scale : number) {
+        context.fillStyle = foreColor
+        context.fillRect(0, -size * scale, size, size * scale)
+    }
+
+    static drawRSURNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        const rSize : number = size / rectSizeFactor
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        context.rotate(Math.PI/2 * sc2)
+        context.strokeRect(-rSize, -rSize, 2 * rSize, 2 * rSize)
+        for (var j = 0; j < rects; j++) {
+            context.save()
+            context.rotate(j * Math.PI / 2)
+            context.translate(-size, -size)
+            DrawingUtil.drawUpRect(context, size - rSize, ScaleUtil.divideScale(sc1, j, rects))
+            context.restore()
+        }
+        context.restore()
     }
 }
 
